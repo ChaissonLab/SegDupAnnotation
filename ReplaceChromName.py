034270b44589a0a9bd41541233fe84a9224f5148
Mark Chaisson <mchaisso@usc.edu>
2021-06-24 15:14:10 -0700

    Adding new file for swapping accession IDs with chrom names so that I don't forget to add later.

---
+++ ReplaceChromName.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+mapFileName = sys.argv[1]
+if mapFileName == "NO_OP":
+    for line in sys.stdin:
+        sys.stdout.write(line)
+else:
+    mapFile=open(mapFileName)
+    nameMap={}
+    for line in mapFile:
+        vals=line.split()
+        nameMap[vals[0]] = vals[1]
+    for line in sys.stdin:
+        vals=line[0]
+        for i in range(0,len(vals)):
+            if vals[i] in nameMap:
+                vals[i] = nameMap[vals[i]]
+        sys.stdout.write("\t".join(vals) + "\n")
+        
