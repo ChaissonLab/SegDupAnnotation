67440aa831e9fc0facb96c822e0a0b7953bfdc68
red <rdagnew@usc.edu>
2021-09-20 14:30:22 -0700

    add stats

--- GetCoverageOfRegions.sh
+++ GetCoverageOfRegions.sh
@@ -2,10 +2,10 @@
 bedFile=$1
 covFile=$2
 meanCov=`cat $3`
-
+sdir=$4
 
 while read line; do
     rgn=`echo $line | awk '{ print $1":"$2"-"$3;}'`
-    tabix $covFile  $rgn | cut -f 4 | stats | awk -v m=$meanCov '{ print $1/m;}'
+    tabix $covFile  $rgn | cut -f 4 | $sdir/stats | awk -v m=$meanCov '{ print $1/m;}'
 done  < $bedFile
    
