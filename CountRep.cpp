b0e096ec88e0a12533b713937313e97cffb9c25e
krabbani3 <krabbani@usc.edu>
2022-08-03 17:51:07 -0700

    Fixed error that counts chars in fasta title lines in nucleotide counts.

--- CountRep.cpp
+++ CountRep.cpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+// purpose: Calculate percent assembly masked.
+// method: Report number of lower and upper case chars in input.
+// input: (assembly) fasta file
+// output: <fasta entry header> <(num lowercase chars)/(num total chars)> <num lowercase chars in entry> <num uppercase chars in entry>
+
 int main() {
 	long int nl=0, nh=0, other=0;
 
@@ -9,25 +15,28 @@ int main() {
 	while (cin) {
 		string line;
 		cin >> line;
-		if (line.size() > 0 and line[0] == '>') {
-			if (prevTitle != "") {
-				int s=nl+nh;
-				if (s > 0) {
-					cout << prevTitle << "\t"<< nl / ((float)s) << "\t" << nl << "\t" << nh << endl;
+		if (line.size() > 0) {
+			if (line[0] == '>') {
+				if (prevTitle != "") {
+					int s=nl+nh;
+					if (s > 0) {
+						cout << prevTitle << "\t"<< nl / ((float)s) << "\t" << nl << "\t" << nh << endl;
+					}
+					else {
+						cout << prevTitle << "\t0\t0\t0" << endl;
+					}
 				}
-				else {
-					cout << prevTitle << "\t0\t0\t0" << endl;
+				nl=0; nh=0;
+				prevTitle = line;
+			} else {
+				for (int i=0; i < line.size(); i++) {
+					if (line[i] >= 'a' && line[i] <= 'z') {
+						nl+=1;
+					}
+					else {
+						nh+=1;
+					}
 				}
-			}
-			nl=0; nh=0;
-			prevTitle = line;
-		}
-		for (int i=0; i < line.size(); i++) {
-			if (line[i] >= 'a' && line[i] <= 'z') {
-				nl+=1;
-			}
-			else {
-				nh+=1;
 			}
 		}
 	}
